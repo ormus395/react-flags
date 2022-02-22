9d34dfe372a7a3a2a95ebde274d3a0d72a0a8f87
ormus395 <ormus395@gmail.com>
2022-02-22 14:44:42 -0700

    Deleted unused vars

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Container from "./layout/Container";
-import CountryList from "./pages/CountryList";
 
 function App() {
   return (
